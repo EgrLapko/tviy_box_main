f208b07ce2aa167792041a23c1e1acc4c8498a1b
Yehor Lapko <egorlapko@gmail.com>
2022-07-13 10:39:22 +0300

    Added Header component and additional styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Main Page Here</div>;
+  return <div></div>;
 };
 
 export default Home;
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,7 +1,89 @@
 import React from "react";
+import Image from "next/image";
+
+import { routes } from "utils/routing";
+import LinkComponent from "components/common/LinkComponent";
+
+const links = [
+  {
+    id: 1,
+    title: "Каталог боксів",
+    route: routes.catalogue(),
+  },
+  {
+    id: 2,
+    title: "Конструктор",
+    route: routes.constructor(),
+  },
+  {
+    id: 3,
+    title: "Доставка і оплата",
+    route: routes.deliveryAndPayments(),
+  },
+  {
+    id: 4,
+    title: "Контакти",
+    route: routes.contacts(),
+  },
+  {
+    id: 5,
+    title: "FAQ",
+    route: routes.faq(),
+  },
+  {
+    id: 6,
+    title: "Корпоративним клієнтам",
+    route: routes.corporateClients(),
+  },
+];
 
 const Header = () => {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <div className="header-left">
+        <div className="header-logo-wrapper">
+          <LinkComponent {...routes.home()}>
+            <Image
+              className="header-logo"
+              src="/static/svg/logo-tb.svg"
+              width={62}
+              height={32}
+              alt="logo"
+            />
+          </LinkComponent>
+        </div>
+        <div className="header-links">
+          {links.map((link) => {
+            return (
+              <div key={link.id} className="header-link">
+                <LinkComponent className="link" {...link.route}>
+                  {link.title}
+                </LinkComponent>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="header-right">
+        <div className="header-cart-button">
+          <button className="button">
+            <span style={{ marginRight: 8 }}>
+              <Image
+                src="/static/svg/cart-black.svg"
+                width={15}
+                height={15}
+                alt="cart-ico"
+              />
+            </span>
+            Кошик
+          </button>
+        </div>
+        <div>
+          <button className="button color-primary">Створити свій бокс</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
